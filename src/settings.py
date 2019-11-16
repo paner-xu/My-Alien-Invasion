6fa90ab62cdc64a90cd7531bf9b941cbc2b32a2b
65jie <65jiee@gmail.com>
2019-11-16 23:44:24 +0800

    format codes and modify changelog

--- src/settings.py
+++ src/settings.py
@@ -10,12 +10,9 @@ class Setting:
 
         self.ship_speed = 1.5
         # bullet settings
-        self.bullet_color = 60,60,60
+        self.bullet_color = (60, 60, 60)
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_speed = 1
 
         self.bullets_allowed = 3
-
-
-

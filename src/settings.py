25d73b0a9a4483fd7b721f37c59a52386c42ef55
paner-xu <13419677763@163.com>
2019-11-19 09:32:59 +0800

    modify the settings and update the changelog

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ class Setting:
         self.ship_speed = 1.5
         
         # bullet settings
-        self.bullet_color = 60,60,60
+        self.bullet_color = (60,60,60)
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_speed = 1.5

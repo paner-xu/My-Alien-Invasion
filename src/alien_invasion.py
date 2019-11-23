938037bfed8ccfcbee2e514f889dc4bf6ffce201
65jie <65jiee@gmail.com>
2019-11-23 14:15:00 +0800

    modify the main runner

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -217,9 +217,3 @@ class AlienInvasion:
         if self.bs.sugar_rect.bottom == self.ship.rect.top:
             self.laser.draw_laser()
         pygame.display.flip()
-
-
-if __name__ == '__main__':
-    # Make a game instance, and run the game.
-    ai = AlienInvasion()
-    ai.run_game()
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from alien_invasion import AlienInvasion
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
